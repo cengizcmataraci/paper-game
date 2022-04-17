63de2a9cfe8b74c2255deadf698c5155bb4489f4
cengizcmataraci <cengizcmataraci@gmail.com>
2022-04-17 17:36:24 +0300

    add selectors

---
+++ src/selectors.js
@@ -0,0 +1,28 @@
+import { selector } from "recoil";
+
+import { gameScore } from "./atoms";
+
+const fetchHighScores = async () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(303);
+    }, 1000);
+  });
+};
+
+const paperSize = selector({
+  key: "paperSize",
+  get: ({ get }) => {
+    const score = get(gameScore);
+    return 100 + score * 5;
+  },
+});
+
+const highScores = selector({
+  key: "highScores",
+  get: async ({ get }) => {
+    return await fetchHighScores();
+  },
+});
+
+export { paperSize, highScores };
